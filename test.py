ef666bd3c7d160a68eb4260b83c4b67a640bde4c
Luke Caldwell <lukecaldwell02@gmail.com>
2018-11-21 09:31:18 +0000

    Added Make_Multiple_Comparison_Plot
    - Function to loop over many normalisation types and give them different
    output file names

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-from main import Make_Comparison_Plot
+from main import Make_Comparison_Plot, Make_Multiple_Comparison_Plot
 
-Make_Comparison_Plot(['154_1.xlsx', '172_1.xlsx'], 'O1s', ['a', 'b'], ['C1s Scan', 'O1s Scan', 'Fe2p Scan'], 'test.pdf')
+Make_Comparison_Plot(['154_1.xlsx', '172_1.xlsx'], 'C1s', ['a', 'b'], ['C1s Scan', 'O1s Scan', 'Fe2p Scan'], 'test.pdf')
