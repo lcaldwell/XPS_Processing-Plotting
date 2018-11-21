c9aa4dd944a5ce42dd400445dd5b820e7773d411
Luke Caldwell <lukecaldwell02@gmail.com>
2018-11-21 09:15:16 +0000

    Automation of finding normalisation values
    - Gets the values from peak table for C-C, M-O, Fe, Total of O, C, Fe

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from XPSProcessing import XPSDataSet
 from plotting import Plot_Comparison
 
-def Make_Comparison_Plot(file_names, normalisations, legend_names, scan_types, output_file_name, colours=None):
-    if not len(file_names) == len(normalisations) == len(legend_names):
+def Make_Comparison_Plot(file_names, normalisation_type, legend_names, scan_types, output_file_name, colours=None):
+    if not len(file_names) == len(legend_names):
         raise Exception
 
     # Make empty lists for each plot
@@ -10,10 +10,9 @@ def Make_Comparison_Plot(file_names, normalisations, legend_names, scan_types, o
 
     for i in range(len(file_names)):
         file_name = file_names[i]
-        normalisation = normalisations[i]
         dataset = XPSDataSet(file_name)
         for j in range(len(scan_types)):
-            data[j].append(dataset.get_scan_data(scan_types[j], normalisation))
+            data[j].append(dataset.get_scan_data(scan_types[j], normalisation_type))
 
     Plot_Comparison(data, scan_types, legend_names, output_file_name)
     
